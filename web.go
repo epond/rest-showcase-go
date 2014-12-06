97d09720e494d5b31a4e4f5a08c2fbd909d827f6
epond <epond@users.noreply.github.com>
2014-12-06 17:26:58 +0000

    Showcase: json decode but not encode

--- web.go
+++ web.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,9 +10,10 @@ import (
 )
 
 func main() {
-	log.Print("Starting...")
+	log.Print("Starting Golang Rest Showcase...")
 	http.HandleFunc("/", HomeHandler)
 	http.HandleFunc("/basic", BasicHandler)
+	http.HandleFunc("/showcase", ShowcaseHandler)
 
 	http.ListenAndServe("localhost:9000", nil)
 }
@@ -21,6 +23,30 @@ func HomeHandler(res http.ResponseWriter, req *http.Request) {
 }
 
 func BasicHandler(res http.ResponseWriter, req *http.Request) {
-	body, _ := ioutil.ReadAll(req.Body)
-	fmt.Fprintln(res, strings.ToUpper(string(body)))
+	reqBody, _ := ioutil.ReadAll(req.Body)
+	fmt.Fprintln(res, strings.ToUpper(string(reqBody)))
+}
+
+func ShowcaseHandler(res http.ResponseWriter, req *http.Request) {
+	reqBody, _ := ioutil.ReadAll(req.Body)
+	log.Printf("request body: %s", string(reqBody))
+	var dat map[string]interface{}
+	if err := json.Unmarshal(reqBody, &dat); err != nil {
+		panic(err)
+	}
+
+	log.Printf("Input: %s", dat["input"].(string))
+
+	md5Out := MD5Out{
+		"todo",
+		"todo",
+	}
+
+	jsonOut, _ := json.Marshal(md5Out)
+	fmt.Fprint(res, string(jsonOut))
+}
+
+type MD5Out struct {
+	md5            string
+	originalString string
 }
